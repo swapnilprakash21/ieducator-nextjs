1f0161610ab58e7a9b9a431dec4d4e4fe02b5807
swapnilprakash21 <swapnil.prakash20@gmail.com>
2022-03-17 10:54:05 +0530

    Created Navbar

---
+++ components/Navbar.js
@@ -0,0 +1,62 @@
+import Link from 'next/link'
+import React, { Component } from 'react'
+import styles from '../styles/Navbar.module.css'
+export default class Navbar extends Component {
+     dectctWidth = () => {
+        let navCollapse = document.getElementById("navCollapse");
+        let mediaQuery = window.matchMedia("(max-width: 640px)");
+        if (mediaQuery.matches) {
+            navCollapse.style.display = 'none';
+            navCollapse.style.height = '0px';
+            return true;
+        }
+        else {
+            navCollapse.style.display = 'block';
+            navCollapse.style.height = 'auto';
+            return true;
+        }
+    
+    }
+    
+    componentDidMount(){
+        this.dectctWidth();
+        window.addEventListener('resize', this.dectctWidth);
+    }
+    
+     toggleCollapse = () => {
+        let navCollapse = document.getElementById("navCollapse");
+        if (navCollapse.style.height === '0px') {
+            navCollapse.style.display = 'block';
+            setTimeout(() => {
+                navCollapse.style.height = '85px';
+            }, 1);
+            return true;
+        }
+        else {
+            navCollapse.style.height = '0px';
+            setTimeout(() => {
+                navCollapse.style.display = 'none';
+            }, 150);
+            return true;
+        }
+    }
+  render() {
+    return (
+
+        <nav className={styles.mainnav}>
+        <ul className={styles.navMainUl}>
+            <li>iEducator</li>
+            <button className={styles.navExpand} onClick={this.toggleCollapse}><i className="fas fa-bars    "></i></button>
+        </ul>
+        <div className={styles.navCollapse} id="navCollapse">
+            <ul>
+                <li><Link href="/">Home</Link></li>
+                <li><Link href="/">Blog</Link></li>
+                <li><Link href="/about">About us</Link></li>
+                <li><Link href="/contact">Contact</Link></li>
+            </ul>
+        </div>
+    </nav>
+    )
+  }
+}
---
+++ pages/_document.js
@@ -0,0 +1,20 @@
+import { Html, Head, Main, NextScript } from 'next/document'
+
+export default function Document() {
+  return (
+    <Html>
+      <Head>
+        <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/6.0.0/css/all.min.css"
+          integrity="sha512-9usAa10IRO0HhonpyAIVpjrylPvoDwiPUiKdWk5t3PyolY1cOd4DSE0Ga+ri4AuTroPR5aQvXU9xC6qOPnzFeg=="
+          crossOrigin="anonymous" referrerpolicy="no-referrer" />
+        <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/6.0.0/css/fontawesome.min.css"
+          integrity="sha512-8Vtie9oRR62i7vkmVUISvuwOeipGv8Jd+Sur/ORKDD5JiLgTGeBSkI3ISOhc730VGvA5VVQPwKIKlmi+zMZ71w=="
+          crossOrigin="anonymous" referrerpolicy="no-referrer" />
+      </Head>
+      <body>
+        <Main />
+        <NextScript />
+      </body>
+    </Html>
+  )
+}
