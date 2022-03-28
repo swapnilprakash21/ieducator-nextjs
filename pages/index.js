64766bb353d488d15a944af0505a9020fc66800d
swapnilprakash21 <swapnil.prakash20@gmail.com>
2022-03-28 12:00:51 +0530

    Updated home page

--- pages/index.js
+++ pages/index.js
@@ -4,13 +4,8 @@ import styles from '../styles/Home.module.css';
 import AOS from 'aos'
 import "aos/dist/aos.css";
 import Typed from 'react-typed';
+
 export default class Index extends Component {
-    constructor() {
-        super();
-        this.state = {
-            slideId: 0
-        }
-    }
     componentDidMount() {
         AOS.init();
     }
@@ -30,14 +25,31 @@ export default class Index extends Component {
                                 <h1 className={styles.headerTitle}>Innovative Solutions For Making Education <span style={{ 'color': '#0dcaf0' }}><Typed strings={['Accessible For All', 'A Reality For All Citizens', 'A Holistic Experience',]} typeSpeed={40} backSpeed={50} loop></Typed></span></h1>
                             </div>
                         </div>
+                    </div>
 
+                    <div className={styles.sdgBox}>
+                        <img src="/img/sdg_logo.png" alt="" className={styles.sdgImg} />
+                        <div className={`${styles.container} ${styles.sdgContainer}`}>
+                            <p className='mb-2'>
+                                We are at the brink of a global transformation. The international community, through the United Nations, has set in motion a historic plan – 17 Sustainable Development Goals – that aims to build a more prosperous, more equal, and more secure world by the year 2030.
+                            </p>
+                            <p className='mb-2'>
+                                The <a href="https://sdgs.un.org/2030agenda" target="_blank" rel="noopener noreferrer" className={styles.sdgLink}>17 SDGs and 169 targets</a> are part of the 2030 Agenda for Sustainable Development adopted by 193 Member States at the UN General Assembly Summit in September 2015, and which came into effect on 1 January 2016. These goals are the result of an unprecedented consultative process that brought national governments and millions of citizens from across the globe together to negotiate and adopt the global path to sustainable development for the next 15 years.
+                            </p>
+                            <div>
+                                <h1 className='mb-2'>India&apos;s Leading Role</h1>
+                                <p>
+                                    The Government of India is strongly committed to the 2030 Agenda, including the SDGs, as evidenced by the statements of the Prime Minister and other senior Ministers at national and international meetings. India’s national development goals and its &quot;<span style={{ "fontStyle": "italic" }}>sab ka saath, sab ka vikas</span>&quot; or &quot;<span style={{ "fontStyle": "italic" }}>development with all, and for all,</span>&quot; policy initiatives for inclusive development converge well with the SDGs, and India will play a leading role in determining the success of the SDGs, globally.   As Prime Minister Narendra Modi noted, &quot;<span style={{ "fontStyle": "italic" }}>These goals reflect our evolving understanding of the social, economic and environmental linkages that define our lives.</span>&quot;.
+                                </p>
+                            </div>
+                        </div>
                     </div>
                     <div className={styles.solBox}>
                         <div className={styles.sol}>
                             <div className={styles.probDescContainerMain}>
                                 <div className={styles.probDescContainer}>
                                     <div className={styles.left}>
-                                        <img src="/img/classroom.svg" alt="" className={styles.problemImage} />
+                                        <img src="/img/qualityEducation.jpg" alt="" className={styles.problemImage} />
                                     </div>
                                     <div className={`${styles.right}`}>
                                         <h2>Possible steps that can be taken to accomplish the India 2030 agenda.</h2>
@@ -90,6 +102,8 @@ export default class Index extends Component {
     }
 }
 
+
+// Fetching the solutions from the ieducator api using getStaticProps
 export async function getStaticProps() {
     const res = await fetch('https://ieducator-api-strapi-sp.herokuapp.com/api/solutions?populate=*&sort=id');
     const data = await res.json();
